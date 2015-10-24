099434f434a06e1e37d58e8783968111d0f0c831
mrdanding <xiashuaiqi@gmail.com>
2015-10-23 19:39:15 -0700

    update struts

---
+++ src/edu/ucsd/H/Test.java
@@ -0,0 +1,7 @@
+package edu.ucsd.H;
+
+/**
+ * @author shuaiqi.xsq, 15/10/23
+ */
+public class Test {
+}
---
+++ src/edu/ucsd/H/action/BaseAction.java
@@ -0,0 +1,36 @@
+package edu.ucsd.H.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.apache.struts2.interceptor.ServletResponseAware;
+import org.apache.struts2.interceptor.SessionAware;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+
+public class BaseAction extends ActionSupport implements SessionAware,  
+        ServletRequestAware, ServletResponseAware {  
+  
+    private static final long serialVersionUID = 1L;
+    public static String SUCCESS = "success";
+    public static String ERROR = "error";
+ 
+    public HttpServletRequest   request;  
+    public HttpServletResponse  response;  
+    @SuppressWarnings("unchecked")  
+    public Map session;  
+  
+    public void setSession(Map session) {  
+        this.session = session;  
+    }  
+  
+    public void setServletRequest(HttpServletRequest request) {  
+       this.request = request;  
+    }  
+  
+    public void setServletResponse(HttpServletResponse response) {  
+       this.response = response;  
+    }}
+
